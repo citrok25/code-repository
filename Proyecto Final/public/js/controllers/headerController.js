2bc0be8845b970c82e09d3af6712278546c8ddcd
Antonio Marchante <tortolita25@gmail.com>
2018-02-20 20:43:13 +0100

    Update headerController.js

--- Proyecto Final/public/js/controllers/headerController.js
+++ Proyecto Final/public/js/controllers/headerController.js
@@ -1,25 +1,24 @@
 myApp.controller('header', ['$scope', '$location','$http', function($scope, $location,$http){
 
 	var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
+    	var ctx = c.getContext("2d");
 
-    /*  < */
-    ctx.moveTo(30,25);
-    ctx.lineTo(10,40);
-    ctx.lineTo(30,55);
+	/*  < */
+	ctx.moveTo(30,25);
+	ctx.lineTo(10,40);
+	ctx.lineTo(30,55);
 
-    /* WikiCode */
-    ctx.font = "bold 28px Courier New";
-    ctx.fillText ("WikiCode", 34,50);
+	/* WikiCode */
+	ctx.font = "bold 28px Courier New";
+	ctx.fillText ("WikiCode", 34,50);
 
-    /* > */
-    ctx.moveTo(170,25);
-    ctx.lineTo(190,40);
-    ctx.lineTo(170,55);
-    ctx.lineWidth = 3;
-    // Define el color para el dibujo canvas
-    ctx.strokeStyle = '#fff';
-    ctx.stroke();
-
-
+	/* > */
+	ctx.moveTo(170,25);
+	ctx.lineTo(190,40);
+	ctx.lineTo(170,55);
+	ctx.lineWidth = 3;
+	
+	// Define el color para el dibujo canvas
+	ctx.strokeStyle = '#fff';
+	ctx.stroke();
 }])
